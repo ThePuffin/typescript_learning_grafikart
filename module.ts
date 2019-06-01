42622913da4e59f1bd78deec78111e3016dac786
Vinz <ballutvincent@gmail.com>
2019-06-01 09:59:11 +0200

    before import lib
    32m45

--- module.ts
+++ module.ts
@@ -1,3 +1,15 @@
+function Component(target) {
+  console.log(target);
+}
+function Componentt(options) {
+  return function component(target) {
+    console.log(options, target);
+  };
+}
+@Component
+@Componentt({
+  selector: ".demo"
+})
 export default class Demod {
   private options;
   constructor(options) {
